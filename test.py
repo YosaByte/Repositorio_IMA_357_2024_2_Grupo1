7c293f4f1de18ffe16deec70e558d8957f28f88a
Josafat Choque <j.choque01@ufromail.cl>
2024-11-11 22:26:24 -0300

    test.py modificado

--- test.py
+++ test.py
@@ -44,14 +44,11 @@ def sim_coseno(vec1, vec2):
     vec1 = [val for val in vec1.values()]
     vec2 = [val for val in vec2.values()]
     dot_prod = 0
-    frec=0
     for i, v in enumerate(vec1):
         dot_prod += v * vec2[i]
-        if v * vec2[i]!=0:
-            frec+=1
     norm_1 = math.sqrt(sum([x**2 for x in vec1]))
     norm_2 = math.sqrt(sum([x**2 for x in vec2]))
-    return dot_prod / (norm_1 * norm_2), frec
+    return dot_prod / (norm_1 * norm_2)
 
 
 #document_BoW_vector = BoW_vec(docs=documentos, tokenizer=tokenizer)
